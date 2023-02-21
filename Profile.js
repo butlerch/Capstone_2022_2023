00c2bd2665137ebbb6f1ffd421baffb348a99b9a
jisu1 <77786557+jisu1@users.noreply.github.com>
2023-02-20 16:43:09 -0800

    Add files via upload
    profile need change, tabs need add

---
+++ Profile.js
@@ -0,0 +1,18 @@
+// State Management & Authentication
+import {useAuth0} from "@auth0/auth0-react";
+
+// Routing & Paths
+import {Navigate} from "react-router-dom";
+import Tabs from "../components/Tabs";
+
+// Style & Components
+import UserData from "../components/UserData";
+
+
+// Component Description: The "User Profile" page.
+export default function Profile() {
+    const {isAuthenticated} = useAuth0();
+    return <>
+        {isAuthenticated ? <Tabs/> : <Navigate to="/"/>}
+    </>
+}
